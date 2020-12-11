891414313062fc9041ac1d7c76115a91caefec1b
seonghun jo <gkswh@jbnu.ac.kr>
2020-12-12 02:59:56 +0900

    added python file

---
+++ ex050.py
@@ -0,0 +1,4 @@
+data = "039490     " # data변수에 039490     이라는 값을 넣는다.
+print(data) # data 값을 출력한다.
+data1 = data.rstrip() # data1 변수에 data의 오른쪽 공백이 제거된 새로운 수를 넣는다.
+print(data1) # data1 값을 출력한다.
---
+++ ex060.py
@@ -0,0 +1,3 @@
+nums = [1, 2, 3, 4, 5] # nums 함수에 리스트 1,2,3,4,5 값을 넣는다.
+average = sum(nums) / len(nums) # sum으로 리스트의 총 값을 len인 리스트의 갯수로 나눈 값을 average 함수에 넣는다.
+print(average) # 갯수를 총 값으로 나눈 평균값을 출력한다.
