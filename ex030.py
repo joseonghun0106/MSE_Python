af61fa9b8fead927b7a394a656b06abf31b94170
seonghun jo <gkswh@jbnu.ac.kr>
2020-12-12 02:59:16 +0900

    added python file

---
+++ ex030.py
@@ -0,0 +1,5 @@
+string = 'abcd' # string이라는 변수에 abcd라는 값을 넣는다.
+string.replace('b', 'B') # replace 함수를 이용해 b를 B로 바꾼다.
+print(string) # string 값을 출력한다.
+a = "결과값은 바뀌지 않는 abcd가 나올 것이다." # 예상한 변수
+print(a) # 정답을 적은 a를 출력한다.
---
+++ ex040.py
@@ -0,0 +1,4 @@
+data = "   삼성전자   " # data 변수에    삼성전자   라는 값을 넣는다.
+print(data) # data 변수 값을 출력한다.
+data1 = data.strip() # strip() 함수를 이용하여 공백을 제거한다.
+print(data1) # data1 변수 값을 출력한다.
